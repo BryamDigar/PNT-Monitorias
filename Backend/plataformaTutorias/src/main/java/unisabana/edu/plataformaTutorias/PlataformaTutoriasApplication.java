6d4ca543f1976465c1816b85aba29fd222cc6a76
BryamDigar <bryamdiaz1010@gmail.com>
2023-05-26 20:51:46 -0500

    Se añaden las bases del proyecto

---
+++ Backend/plataformaTutorias/src/main/java/unisabana/edu/plataformaTutorias/PlataformaTutoriasApplication.java
@@ -0,0 +1,13 @@
+package unisabana.edu.plataformaTutorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaTutoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaTutoriasApplication.class, args);
+	}
+
+}
---
+++ Backend/plataformaTutorias/src/test/java/unisabana/edu/plataformaTutorias/PlataformaTutoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package unisabana.edu.plataformaTutorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaTutoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
